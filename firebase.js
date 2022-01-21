656285b957b19adee49f736c62d035dae35cac0e
Abdul Basit <basitirfan760@gmail.com>
2022-01-21 20:02:41 +0500

    App complete

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps , getApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBpX0NBY37NgkXzAjjRc9YB-pFb0NUJRXs",
   authDomain: "instagram-clone-e8e90.firebaseapp.com",
   projectId: "instagram-clone-e8e90",
   storageBucket: "instagram-clone-e8e90.appspot.com",
   messagingSenderId: "940655809958",
-  appId: "1:940655809958:web:aea7f8040b996f2c366b67"
+  appId: "1:940655809958:web:3372b80a477561d6366b67"
 };
 
 // Initialize Firebase
