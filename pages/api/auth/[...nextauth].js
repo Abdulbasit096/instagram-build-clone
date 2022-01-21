9342bd1166ff0caa9270cecb7f8b4112c9d7d86b
Abdul Basit <basitirfan760@gmail.com>
2022-01-21 09:32:12 +0500

    FIXED

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,13 +9,8 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-<<<<<<< HEAD
-  pages: {
-    signIn: "/auth/signin",
-  },
-=======
 
->>>>>>> 58d00df33846c3e625f9cee4a9db3fd662625738
+
   callbacks: {
     async session({ session, token, user }) {
       session.user.username = session.user.name
